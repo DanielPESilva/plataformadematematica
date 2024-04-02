a2faef02c915bfb989d24e9be8de692495862382
Thalysson Emanoel C. Vargas <thalysson140105@gmail.com>
2024-04-02 11:49:15 -0400

    Foram adicionadas as classes, os métodos construtores, os get e setters, por fim o banco agora tem o Script das tabelas

---
+++ ClassesEMetodos/Questao.java
@@ -0,0 +1,59 @@
+package ClassesEMetodos;
+
+public class Questao {
+    private int id;
+    private int posicao;
+    private String titulo;
+    private String pdf;
+    private String link_video;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(int posicao) {
+        this.posicao = posicao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+    public String getLink_video() {
+        return link_video;
+    }
+
+    public void setLink_video(String link_video) {
+        this.link_video = link_video;
+    }
+
+    public Questao(int id, int posicao, String titulo, String pdf, String link_video) {
+        this.id = id;
+        this.posicao = posicao;
+        this.titulo = titulo;
+        this.pdf = pdf;
+        this.link_video = link_video;
+
+
+    }
+}
