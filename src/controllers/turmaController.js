b6092d37d62c0c545c64e33aa15e695251ef07e3
Thalysson Emanoel Carnevale Vargas <thalysson140105@gmail.com>
2024-08-13 21:10:26 -0400

    Fazendo requisições no listar

--- src/controllers/turmaController.js
+++ src/controllers/turmaController.js
@@ -5,20 +5,45 @@ import CommonResponse from '../utils/commonResponse.js';
 
 env.config();
 
+
 class TurmaController{
-  
+
+  /**
+   * @listar Listará todas as turmas, caso não exista turma dará erro
+   * @page O primeiro é quantidade de tabelas que aparecerão e o segundo é a quantidade de colunas que cada tabela terá...
+   * @error200 Deu certo :)
+   * @error400 sintaxe de requisição mal formada ou enquadramento de mensagem de requisição inválida
+   * @error500 problema generalizado no servidor interno da api
+   * @pagination Como o page irá operar
+   *
+   */
   static listar = async (req, res) => {
     try {
-      const response = await prisma.turma.findMany()
-      res.status(200).json({ response: response })
-    } catch (error) {
-      res.status(400).json({
-        error: true,
-        code: 400,
-        message: error.message
+      const usuarios = await usuarioRepository.findById(id);
+  
+  if (!usuarios) {
+    return res.status(404).json(CommonResponse.notFound('Usuário não encontrado.'));
+  }
+
+  const { titulo, page = 1, perPage = 10 } = req.query;
+  const { turmas, total } = await turmaService.listar(titulo, usuarios.nome, parseInt(page), parseInt(perPage));
+
+  if (turmas.length === 0) {
+    return res.status(400).json(CommonResponse.notFound(messages.validationGeneric.resourceNotFound('Turmas')));
+  }
+
+  const resultado = {
+    titulo,
+    usuarios 
+  };
+  
+    } catch (err) {
+      if (process.env.DEBUGLOG === 'true') {
+        console.log(err);
       }
-      )
+      return res.status(500).json(CommonResponse.serverError());
     }
+
   }
 
   static listarPorID = async (req, res) => {
--- src/services/turmaService.js
+++ src/services/turmaService.js
@@ -1,32 +1,29 @@
 import turmaRepository from "../repositores/turmaRepository.js";
 
-class turmaService{
-    async listar(filtro){
-        // Busca as turmas com base no filtro
-        const turmas = await turmaRepository.findMany(filtro);
-
-        // Filtra as turmas que possuem pelo menos um aluno associado
-        const turmasComAlunos = turmas.filter(turma => turma.aluno_id && turma.aluno_id.length > 0);
-
-        // Verifica se há turmas com alunos
-        if (turmasComAlunos.length === 0) {
-            throw new Error("Nenhuma turma com pelo menos um aluno foi encontrada.");
-        }else{
-          // Retorna as turmas que possuem pelo menos um aluno
-          return turmasComAlunos;
-        }
-
+class turmaService {
+  async listar(filtro) {
+    try {
+      const response = await prisma.turma.findMany();
+      res.status(200).json({ response: response });
+    } catch (error) {
+      res.status(400).json({
+        error: true,
+        code: 400,
+        message: error.message,
+      });
     }
+    const turmas = await turmaRepository.findMany(filtro);
+    return turmas;
     
+  }
+
   async listarPorID(id) {
     // teste se o id é um número
     if (isNaN(id)) {
-        console.log(id)
-      throw new Error('ID deve ser um número inteiro)');
+      console.log(id);
+      throw new Error("ID deve ser um número inteiro)");
     }
     return turmaRepository.findById(id);
   }
-
-    
 }
 export default new turmaService();
