8c4434b0b18ee7ea6cc0e545489da73a1c35c332
Thalysosn Emanoel C. Vargas <thalysson140105@gmail.com>
2024-08-20 15:32:52 -0400

    WIP

---
+++ src/docs/routes/auth.js
@@ -0,0 +1,32 @@
+import authSchemas from "../schemas/authSchema.js";
+import commonResponses from "../schemas/commonResponses.js";
+
+const authRoutes = {
+    "/login": {
+        post: {
+            tags: ["Auth"],
+            summary: "Realiza login",
+            requestBody: {
+                content: {
+                    "application/json": {
+                        schema: {
+                            type: "object",
+                            properties: {
+                                email: { type: "string", example: "john@gmail.com" },
+                                password: { type: "string", example: "ABCDabcd1234" }
+                            },
+                            required: ["email", "password"]
+                        }
+                    }
+                }
+            },
+            responses: {
+                200: commonResponses[200]("#/components/schemas/RespostaLogin"),
+                400: commonResponses[400](),
+                401: commonResponses[401]()
+            }
+        }
+    }
+};
+
+export default authRoutes;
