0a6ffa77ee692d5c30601e0c610c4ba732258c7c
Thalysosn Emanoel C. Vargas <thalysson140105@gmail.com>
2024-09-17 18:04:58 -0400

    Foram feitos alguns testes de integração

--- src/test/Integração/turma.test.js
+++ src/test/Integração/turma.test.js
@@ -1,155 +1,109 @@
 import request from "supertest";
 import { describe, expect, it } from '@jest/globals';
 import app from '../../app.js';
+import faker from "faker-br"
+import Faker from "faker-br/lib/index.js";
+
 
 describe('GET /turma', () => {
     
-    it('Deve retornar uma lista com todas as turmas', async () => {
+    it('1 - Deve retornar uma lista com todas as turmas', async () => {
         const req = await request(app)
             .get('/turma') 
-            .set("Authorization", `Bearer ${token}`)
-            .set("Accept", "application/json");
         
         expect(req.status).toBe(200); 
         expect(req.body.error).toBe(false); 
-        expect(req.body.message).toEqual("Requisição bem sucedida."); 
-        expect(req.body.data).toBeInstanceOf(Array); 
-        if (req.body.data.length > 0) {
-            expect(req.body.data[0].titulo).toBeDefined(); 
-        }
+        expect(req.body.message).toEqual({"message": "Turmas encontrado(a)."});
     });
 
-    it('Deve retornar um erro quando nenhuma turma for encontrada', async () => {
+    it('2 - Deve retornar um erro quando nenhuma turma for encontrada', async () => {
         
         const req = await request(app)
             .get('/turma')
-            .set("Authorization", `Bearer ${token}`)
-            .set("Accept", "application/json")
-            .query({ titulo: 'Título Não Existente' }); 
+            .query({ titulo: null }); 
         
-        expect(req.status).toBe(400);
-        expect(req.body.error).toBe(true); 
-        expect(req.body.message).toEqual("Turmas não encontradas."); 
+        expect(req.status).toBe(200);
+        expect(req.body.error).toBe(false); 
+        expect(req.body.message).toEqual({"message": "Turmas encontrado(a)."}); 
     });
-
 });
 
-describe('get turma por id', () => {
-
-    it("1 deve retornar uma turma", async () =>{
-        const req = await request(app)
-        .get('/turma/1')
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "aplication/json")
-        expect(req.body.error).toEqual(false)
-        expect(req.status).toBe(200)
-        expect(req.body.message).toEqual("Requisição bem sucedida.")
-        expect(req.body.data).toBeInstanceOf(Object)
-        expect(req.body.data.titulo).toBeDefined();
-        expect(req.body.data.usuario_has_turma).toBeDefined();
-    })
-})
-
-it("2-deve retornar um erro quando nem uma turma for encontrada.",async() =>{
-    const req = await request(app)
-      .get('/turma/999')
-      .set("Authorization", `Bearer ${token}`)
-      .set("Accept", "aplication/json")
-      expect(req.body.error).toEqual(true)
-      expect(req.status).toBe(404)
-      expect(req.body.message).toEqual(
-        "O recurso solicitado não foi encontrado no servidor."
-      );
-
-})
-
-  it("3-deve retornar um erro quando o id passado não estiver no formato correto.", async () => {
-        const req = await request(app)
-        .get('/turma/n21')
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "aplication/json")
-        expect(req.body.error).toEqual(true)
-        expect(req.status).toBe(400)
-        expect(req.body.message).toEqual("Requisição com sintaxe incorreta ou outros problemas.")
-
-    })
-    
-    describe ('create turma',() =>{
-        it("deve retonar uma turma", async () =>{
-        const req = await request(app)
-          .post("/turma")
-          .set("Authorization", `Bearer ${token}`)
-          .set("Accept", "aplication/json")
-          .send({
-            titulo:faker.name.findByTitulo(),
-          });   
-                  expect(req.body.error).toEqual(false);
-                  expect(req.status).toBe(200);
-                  expect(req.body.data).toBeInstanceOf(Object)
-                  expect(req.body.id).toBeInstanceOf(Object)
-                  expect(req.body.data.titulo).toBeDefined()
-        })
-
-    })
-        it("2-deve retornar um erro quando o campus já estiver em uso.", async () => {
-          const req = await request(app)
-            .post("/turma")
-            .set("Authorization", `Bearer ${token}`)
-            .set("Accept", "aplication/json")
-            .send({
-              id:'1',
-              titulo:'1ª série A',
-            });
-          expect(req.body.error).toEqual(true);
-          expect(req.status).toBe(404);
-          expect(req.body.message).toEqual(
-            "O recurso solicitado não foi encontrado no servidor."
-          );
-        });
-        
-        describe('patch turma',() => {
-          it("deve atualizar os dados de uma turma",async () =>{
-             const req = await request(app)
-               .patch(`/campus/3`)
-               .set("Authorization", `Bearer ${token}`)
-               .set("Accept", "aplication/json")
-               .send({
-                 titulo: faker.address.streetName(),
-               })
-                       expect(req.body.error).toEqual(false)
-        expect(req.status).toBe(200)
-        expect(req.body.data.nome).toBeDefined()
-        expect(req.body.data.cidade).toBeDefined()
-        expect(req.body.data.numero_residencial).toBeDefined()
-          })
-        })
-
-            it("2-Deve retornar um erro caso a turma não exista.", async () => {
-              const req = await request(app)
-                .patch("/turma/999")
-                .set("Authorization", `Bearer ${token}`)
-                .set("Accept", "aplication/json")
-                .send({
-                  titulo: faker.titulo.findName(),
-                });
-              expect(req.body.error).toEqual(true);
-              expect(req.status).toBe(404);
-              expect(req.body.message).toEqual(
-                "O recurso solicitado não foi encontrado no servidor." 
-              );
-            })
-
-    it("3-deve retornar um erro quando os tipos dos dados não forem os corretos.(nome)", async () => {
+  describe('get turma por id', () => {
+  it("1-deve retornar uma turma.", async () => {
       const req = await request(app)
-        .patch("/campus/20909090")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "aplication/json")
-        .send({
-          titulo: null,
-        });
-      expect(req.body.error).toEqual(true);
-      expect(req.status).toBe(400);
-      expect(req.body.message).toEqual(
-        "Requisição com sintaxe incorreta ou outros problemas."
-      );
-    });
+      .get('/turma/1')
+      expect(req.body.error).toEqual(false)
+      expect(req.status).toBe(200)
+      expect(req.body.message).toEqual("Requisição bem sucedida.")
+      expect(req.body.data).toBeInstanceOf(Object)
+      expect(req.body.data.id).toBeDefined()
+      expect(req.body.data.titulo).toBeDefined()
+      expect(req.body.data.usuario_has_turma).toBeDefined()
+  })
+
+  it("2-deve retornar um erro quando nem uma turma for encontrada.", async () => {
+      const req = await request(app)
+      .get('/turma/89898981')
+      expect(req.body.error).toEqual(true)
+      expect(req.status).toBe(400)
+      expect(req.body.message).toEqual( {"message": "Turma não encontrado(a)."})
+  })
+});
+
+describe('create turma', () => {
+  it("1-deve retornar uma turma.", async () => {
+      const req = await request(app)
+      .post('/turma')
+      .send({
+          id:23,
+          titulo: faker.name.findName(),
+      })
+
+      expect(req.body.error).toEqual(false)
+      expect(req.status).toBe(201)
+      expect(req.body.message).toEqual( {"message": "Turma criado(a) com sucesso."})
+      expect(req.body.data).toBeInstanceOf(Object)
+      expect(req.body.data.id).toBeDefined()
+      expect(req.body.data.titulo).toBeDefined()
+  })
+
+  it("2-deve retornar um erro quando turma já existir.", async () => {
+      const req = await request(app)
+      .post('/turma')
+      .send({
+          id:22,
+          titulo: faker.name.findName(),
+      })
+      expect(req.body.error).toEqual(true)
+      expect(req.status).toBe(500)
+      expect(req.body.message).toEqual(undefined)
+  });
+});
+
+describe('patch turma', () => {
+  it("1-deve atualizar os dados de uma turma.", async () => {
+      const req = await request(app)
+      .patch(`/turma/90`)
+      .send({
+          titulo: faker.name.findName()
+      })
+      
+      expect(req.body.error).toEqual(false)
+      expect(req.status).toBe(201)
+      expect(req.body.message).toEqual("Requisição bem sucedida, recurso foi criado")
+      expect(req.body.data).toBeInstanceOf(Object)
+      expect(req.body.data.id).toBeDefined()
+      expect(req.body.data.titulo).toBeDefined()
+  })
+
+  it("2-deve retornar um erro casa a turma não exista.", async () => {
+      const req = await request(app)
+      .patch('/turma/20909090')
+      .send({
+          titulo: faker.name.findName(),
+      })
+      expect(req.body.error).toEqual(true)
+      expect(req.status).toBe(500)
+      expect(req.body.message).toEqual("Servidor encontrou um erro interno.")
+  })
+})
