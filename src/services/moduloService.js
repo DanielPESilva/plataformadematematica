9853928fd10c3ae782dd837c45a063308c599883
Vitor Gabriel <vitorgabrielvha3@gmail.com>
2024-10-18 21:03:33 -0400

    criei a rota post de modulos, falta testar

--- src/services/moduloService.js
+++ src/services/moduloService.js
@@ -1,4 +1,5 @@
 import moduloRepository from "../repositories/moduloRepository.js";
+import prisma from '../prismaClient';
 
 class ModuloService {
     async listar() {
@@ -11,8 +12,45 @@ class ModuloService {
     }
 
     async inserir(data) {
-        return await moduloRepository.create(data);
-    }
+        const inserir = async (req, res) => {
+            try {
+              const parsedData = moduloSchema.parse(req.body);
+        
+              const moduloCreated = await prisma.modulos.create({
+                data: {
+                  mod_id: parsedData.mod_id,
+                  mod_tema: parsedData.mod_tema,
+                  mod_descricao: parsedData.mod_descricao,
+                  mod_pdf: parsedData.mod_pdf,
+                  mod_linkVideo: parsedData.mod_linkVideo,
+                },
+              });
+        
+              return res.status(201).json({
+                error: false,
+                code: 201,
+                message: 'Módulo criado.',
+                data: moduloCreated,
+              });
+            } catch (error) {
+              if (error instanceof z.ZodError) {
+                return res.status(400).json({
+                  error: true,
+                  code: 400,
+                  message: 'Erro de validação.',
+                  details: error.errors,
+                });
+              }
+              console.error(error);
+              return res.status(500).json({
+                error: true,
+                code: 500,
+                message: 'Erro interno.',
+              });
+            }
+          };
+                return await moduloRepository.create(data);
+            }
 
     async atualizar(id, data) {
         return await moduloRepository.update(id, data);
